b1e68573cde284949fe3bee8a77e2d0b38d72bf5
TheCodeLibrary-Chapter1 <36452292+TheCodeLibrary-Chapter1@users.noreply.github.com>
2021-01-09 20:10:02 +0530

    Add files via upload

--- SAS_to_R.R
+++ SAS_to_R.R
@@ -21,3 +21,12 @@ anaie<-anaie[order(anaie$ietestcd, na.last=TRUE) , ]
 #left joining 
 anaie1<-merge(x = anaie, y = iesheet, by = "ietestcd",all.x = TRUE)
 
+df %>%
+  mutate(col0 = str_trim(trt01p, side = c("both")),
+         col1 = str_trim(subjid, side = c("both")),
+         col2 = if_else(trt01p == "Dummy A", NA, if_else(is.null(trtstd), "No", "Yes")),
+         col3 = if_else("IN" %in% IETESTCD, substr(IETESTCD, 3, 6), NA),
+         inc = if_else("IN" %in% IETESTCD, paste0(substr(IETESTCD, 1, 1), col3), NA),
+         col4 = if_else("EX" %in% IETESTCD, substr(IETESTCD, 3, 6), NA),
+         inc = if_else("EX" %in% IETESTCD, paste0(substr(IETESTCD, 1, 1), col4), NA)
+  )
